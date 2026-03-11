2fb4497f6f4507e34a2df9948d5f12547b61c836
Congrui Gao <congruigao@Congruis-MacBook-Pro.local>
2026-03-10 22:32:07 -0700

    initial social-ai project

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = "token";
+
+export const BASE_URL = "https://eternal-skyline-487003-r7.uw.r.appspot.com";
+
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
